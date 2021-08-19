2da163f68c4608093cff9b3eea841ea2d4d9e712
Czimer Kristóf <czimer.kristof@outlook.com>
2021-08-19 22:07:17 +0200

    Add project files.

---
+++ SingletonTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingletonTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
